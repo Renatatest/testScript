e807aef1d0a34269f4e43008f8e8c9c2420955a7
Renatatest <14bisestudos@gmail.com>
2021-07-26 22:37:04 -0300

    descrição breve do commit

---
+++ src/test/java/tests/MagazineTest.java
@@ -0,0 +1,33 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class MagazineTest {
+     //sem BDD
+    
+    @Test
+    public void testPesquisaProduto(){
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+       driver.get("https://www.magazineluiza.com.br/");
+       driver.findElement(By.id("inpHeaderSearch")).sendKeys("Wife, Just Let Go - Robert Briggs Associates");
+       driver.findElement(By.id("btnHeaderSearch")).click();
+
+       driver.findElement(By.className("product-li")).click();
+       String status = driver.findElement(By.className("unavailable__product-title")).getText();
+        Assert.assertEquals("Não disponível",status);
+        driver.quit();
+
+    }
+}
